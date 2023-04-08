8240b1ae51a2eb6514d2c10eef600c971f0d4695
Jeremy M Rivera <fangman09@gmail.com>
2023-04-08 18:48:31 -0400

    contexts and fetch err

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -17,18 +17,41 @@ class ShopProvider extends Component {
     isMenuOpen: false,
   };
 
-  createCheckout = async () => {};
+  componentDidMount() {
+    if (localStorage.checkout_id) {
+      this.fetchCheckout(localStorage.checkout_id);
+    } else {
+      this.createCheckout();
+    }
+  }
 
-  fetchCheckout = async () => {};
+  createCheckout = async () => {
+    const checkout = await client.checkout.create();
+    localStorage.setItem("checkout-id", checkout.id);
+    this.setState({ checkout: checkout });
+  };
+
+  fetchCheckout = (checkoutId) => {
+    client.checkout.fetch(checkoutId).then((checkout) => {
+      this.setState({ checkout: checkout });
+    });
+  };
 
   addItemToCheckout = async () => {};
 
   removeLineItem = async (lineItemIdsToRemove) => {};
 
-  fetchAllProducts = async () => {};
+  fetchAllProducts = async () => {
+    const products = await client.product.fetchAll();
+    this.setState({ products: products });
+  };
 
-  fetchProductWithHandle = async (handle) => {};
+  fetchProductWithHandle = async (handle) => {
+    const product = await client.product.fetchByHandle(handle);
+    this.setState({ product: product });
 
+    return product;
+  };
   closeCart = () => {};
 
   openCart = () => {};
@@ -38,10 +61,25 @@ class ShopProvider extends Component {
   openMenu = () => {};
 
   render() {
-    return <ShopContext.Provider>{this.props.children}</ShopContext.Provider>;
+    return (
+      <ShopContext.Provider
+        value={{
+          ...this.state,
+          fetchAllProducts: this.fetchAllProducts,
+          fetchProductWithHandle: this.fetchProductWithHandle,
+          closeCart: this.closeCart,
+          openCart: this.openCart,
+          closeMenu: this.closeMenu,
+          openMenu: this.openMenu,
+          addItemToCheckout: this.addItemToCheckout,
+          removeLineItem: this.removeLineItem,
+        }}
+      >
+        {this.props.children}
+      </ShopContext.Provider>
+    );
   }
 }
-
 const ShopConsumer = ShopContext.Consumer;
 
 export { ShopConsumer, ShopContext };
